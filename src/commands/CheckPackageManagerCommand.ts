34d2962dec09fb238d2e262d161a05e670d13e9e
Eric King <eric@webdeveric.com>
2022-12-27 13:32:33 -0600

    refactor: check if package is a dependency

--- src/commands/CheckPackageManagerCommand.ts
+++ src/commands/CheckPackageManagerCommand.ts
@@ -1,8 +1,6 @@
 import { satisfies } from 'semver';
 
-import {
-  getConfiguredPackageManager, parsePackageManager, parsePackageManagerUserAgent,
-} from '@src/utils.js';
+import { getConfiguredPackageManager, parsePackageManager, parsePackageManagerUserAgent } from '@src/utils.js';
 import { CustomCommand } from './CustomCommand.js';
 import { ExitCodes } from '@src/types.js';
 
@@ -10,47 +8,100 @@ import type Application from '@src/Application.js';
 import type { Command } from 'commander';
 import type { PackageMangerDetails } from '@src/types.js';
 
+export type CheckPackageManagerOptions = {
+  info?: boolean;
+  debug?: boolean;
+};
+
 export class CheckPackageManagerCommand extends CustomCommand {
   register(app: Application): Command {
     const command = app
       .command('check', { isDefault: true })
       .description('Check that the correct package manager is being used.')
       .argument('[packageManager]', 'Package manager name and version')
+      .option('--info', 'Print info messages', false)
       .option('--debug', 'Print debug messages', false)
       .action(this.action.bind(this));
 
     return command;
   }
 
-  checkPackageManagerVersion(
-    currentPackageManager: PackageMangerDetails | undefined,
-    configuredPackageManager: PackageMangerDetails | undefined,
-    debug = false,
-  ): {
-    code: number;
-    messages: string[];
-  } {
+  getMessages(
+    code: number,
+    context: {
+      options: CheckPackageManagerOptions;
+      calledFromDependency: boolean;
+      packageManagerArg: string | undefined;
+      configuredPackageManager: PackageMangerDetails | undefined;
+      currentPackageManager: PackageMangerDetails | undefined;
+    },
+  ): string[] {
     const messages: string[] = [];
 
-    let code = ExitCodes.OK;
+    if (context.options.debug) {
+      const npmEnv = Object.entries(process.env)
+        .filter(([name]) => name.startsWith('npm_'))
+        .sort((left, right) => left[0].localeCompare(right[0]))
+        .map(([key, value]) => `  ${key}: ${value}`)
+        .join('\n');
 
-    if (! currentPackageManager) {
       messages.push(
-        'You\'re doing it wrong. Call `check-package-manager` from `preinstall` script in your package.json file.',
+        `cwd: ${process.cwd()}`,
+        `calledFromDependency: ${context.calledFromDependency}`,
+        `Package manager environment variables:${npmEnv ? `\n${npmEnv}` : ' NONE'}`,
       );
 
-      code |= ExitCodes.DoingItWrong;
+      if (typeof context.packageManagerArg === 'undefined') {
+        messages.push(
+          'Using "packageManager" property from your package.json to determine the configured package manager.',
+        );
+      }
+
+      messages.push(
+        context.configuredPackageManager
+          ? `Configured package manager: ${context.configuredPackageManager.name}@${context.configuredPackageManager.version}`
+          : `Configured package manager: ${context.configuredPackageManager}`,
+        context.currentPackageManager
+          ? `Current package manager: ${context.currentPackageManager.name}@${context.currentPackageManager.version}`
+          : `Current package manager: ${context.currentPackageManager}`,
+      );
     }
 
-    if (debug) {
-      configuredPackageManager &&
-        messages.push(
-          `[DEBUG] Configured package manager: ${configuredPackageManager?.name}@${configuredPackageManager?.version}`,
-        );
-      currentPackageManager &&
-        messages.push(
-          `[DEBUG] Current package manager: ${currentPackageManager?.name}@${currentPackageManager?.version}`,
-        );
+    if (typeof context.currentPackageManager === 'undefined') {
+      messages.push(
+        "You're doing it wrong. Call 'check-package-manager' from 'preinstall' script in your package.json file.",
+      );
+    }
+
+    if (code & ExitCodes.WrongPackageManager && context.configuredPackageManager && context.currentPackageManager) {
+      messages.push(
+        `${process.env.npm_package_name} should be using ${context.configuredPackageManager.name} version ${context.configuredPackageManager.version} instead of ${context.currentPackageManager.name} version ${context.currentPackageManager.version}`,
+      );
+
+      if (!context.packageManagerArg) {
+        messages.push(`Try running "corepack enable" then "${context.configuredPackageManager.name} install"`);
+      }
+    }
+
+    if (context.options.info && code === ExitCodes.OK) {
+      messages.push(
+        context.calledFromDependency
+          ? `It looks like 'check-package-manager' was called from a dependency. Not running check for ${process.env.npm_package_name}.`
+          : `${process.env.npm_package_name} is using the correct package manager.`,
+      );
+    }
+
+    return messages;
+  }
+
+  checkPackageManager(
+    currentPackageManager: PackageMangerDetails | undefined,
+    configuredPackageManager: PackageMangerDetails | undefined,
+  ): ExitCodes {
+    let code = ExitCodes.OK;
+
+    if (!currentPackageManager) {
+      code |= ExitCodes.DoingItWrong;
     }
 
     if (configuredPackageManager && currentPackageManager) {
@@ -58,56 +109,53 @@ export class CheckPackageManagerCommand extends CustomCommand {
         code |= ExitCodes.WrongPackageManagerName;
       }
 
-      if (configuredPackageManager.version && currentPackageManager.version && ! satisfies(currentPackageManager.version, configuredPackageManager.version)) {
+      if (
+        configuredPackageManager.version &&
+        currentPackageManager.version &&
+        !satisfies(currentPackageManager.version, configuredPackageManager.version)
+      ) {
         code |= ExitCodes.WrongPackageManagerVersion;
-      }
-
-      if (code & ExitCodes.WrongPackageManager) {
-        messages.push(
-          `You should be using ${configuredPackageManager.name} version ${configuredPackageManager.version} instead of ${currentPackageManager.name} version ${currentPackageManager.version}`,
-        );
       }
     }
 
-    return { code, messages };
+    return code;
+  }
+
+  isDependency(cwd: string): boolean {
+    return /\/(node_modules|_cacache)\/.+/.test(cwd);
   }
 
   async action(
     packageManagerArg: string | undefined,
-    options: {
-      debug?: boolean;
-    },
+    options: CheckPackageManagerOptions,
     cmd: Command,
   ): Promise<void> {
-    const currentPackageManager = parsePackageManagerUserAgent(process.env.npm_config_user_agent);
-    const configuredPackageManager = packageManagerArg ? parsePackageManager(packageManagerArg) : await getConfiguredPackageManager();
+    const calledFromDependency = this.isDependency(process.cwd());
 
-    const { code, messages } = this.checkPackageManagerVersion(
-      currentPackageManager,
-      configuredPackageManager,
-      options.debug,
-    );
+    let currentPackageManager: PackageMangerDetails | undefined;
+    let configuredPackageManager: PackageMangerDetails | undefined;
+    let code = ExitCodes.OK;
 
-    if (process.env.NODE_ENV === 'development') {
-      if (options.debug) {
-        console.debug({
-          npmEnv: Object.fromEntries(
-            Object.entries(process.env)
-              .filter(([ name ]) => name.startsWith('npm_'))
-              .sort((left, right) => left[ 0 ].localeCompare(right[ 0 ])),
-          ),
-        });
-      }
+    if (!calledFromDependency) {
+      currentPackageManager = parsePackageManagerUserAgent(process.env.npm_config_user_agent);
+
+      configuredPackageManager = packageManagerArg
+        ? parsePackageManager(packageManagerArg)
+        : await getConfiguredPackageManager();
+
+      code = this.checkPackageManager(currentPackageManager, configuredPackageManager);
     }
 
     process.exitCode = code;
 
-    if (code !== ExitCodes.OK && ! packageManagerArg && configuredPackageManager && currentPackageManager) {
-      messages.push(`Try running "corepack enable" then "${configuredPackageManager.name} install"`);
-    }
+    const messages = this.getMessages(code, {
+      options,
+      calledFromDependency,
+      packageManagerArg,
+      configuredPackageManager,
+      currentPackageManager,
+    });
 
-    messages.forEach(message =>
-      cmd.configureOutput().writeOut?.(`${message}\n\n`),
-    );
+    messages.forEach(message => cmd.configureOutput().writeOut?.(`${message}\n`));
   }
 }
