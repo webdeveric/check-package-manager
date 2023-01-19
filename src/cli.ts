0c1477bf45634799c91218b5808100eb98bc8f65
Eric King <eric@webdeveric.com>
2023-01-19 12:14:11 -0600

    fix: warn if `parseArgs` is not a function

--- src/cli.ts
+++ src/cli.ts
@@ -1,30 +1,34 @@
 #!/usr/bin/env node
-import { parseArgs } from 'node:util';
+import util from 'node:util';
 
 import { CheckPackageManager } from './CheckPackageManager.js';
 import { CliError } from './CliError.js';
 import { ExitCodes } from './types.js';
 
 try {
-  const args = parseArgs({
-    allowPositionals: true,
-    options: {
-      info: {
-        type: 'boolean',
-        short: 'i',
-        default: false,
+  if (typeof util.parseArgs === 'function') {
+    const args = util.parseArgs({
+      allowPositionals: true,
+      options: {
+        info: {
+          type: 'boolean',
+          short: 'i',
+          default: false,
+        },
+        debug: {
+          type: 'boolean',
+          short: 'd',
+          default: false,
+        },
       },
-      debug: {
-        type: 'boolean',
-        short: 'd',
-        default: false,
-      },
-    },
-  });
+    });
 
-  const check = new CheckPackageManager(args.positionals.at(0), args.values);
+    const check = new CheckPackageManager(args.positionals.at(0), args.values);
 
-  await check.run();
+    await check.run();
+  } else {
+    console.warn('util.parseArgs is not a function. Please use Node JS >= 16.17');
+  }
 } catch (error) {
   if (error instanceof CliError) {
     process.exitCode = error.code;
