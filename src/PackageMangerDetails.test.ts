755f58404d87064c44edc8585bee705115fce059
Eric King <eric@webdeveric.com>
2023-12-24 08:01:23 -0600

    chore: allow build meta data in package manager string

--- src/PackageMangerDetails.test.ts
+++ src/PackageMangerDetails.test.ts
@@ -1,44 +1,143 @@
 import { describe, it, expect } from 'vitest';
 
 import { PackageMangerDetails } from './PackageMangerDetails.js';
-import { ComparisonResults, type PackageManagerString } from './types.js';
+import { ComparisonResults } from './types.js';
 
 describe('PackageMangerDetails', () => {
   it('Represents a version of a package manager', () => {
-    expect(new PackageMangerDetails('npm', '9.2.0')).toEqual(
+    expect(
+      new PackageMangerDetails({
+        name: 'npm',
+        version: '1.2.3',
+        major: 1,
+        minor: 2,
+        patch: 3,
+      }),
+    ).toEqual(
       expect.objectContaining({
         name: 'npm',
-        version: '9.2.0',
+        version: '1.2.3',
+        major: 1,
+        minor: 2,
+        patch: 3,
       }),
     );
   });
 
   it('Can be created from a PackageManagerString', () => {
-    expect(PackageMangerDetails.create('npm@9.2.0')).toEqual(
+    expect(new PackageMangerDetails('npm@9.2.0')).toEqual(
       expect.objectContaining({
         name: 'npm',
-        version: '9.2.0',
+        major: 9,
+        minor: 2,
+        patch: 0,
       }),
     );
 
-    expect(() => {
-      PackageMangerDetails.create('@9.2.0' as PackageManagerString);
-    }).toThrow();
+    expect(
+      new PackageMangerDetails('pnpm@8.13.0+sha256.fbcf256db6d06bc189e31df34b3ed61220f3ba9f78a2ca8fe7be0fce4670dbd3'),
+    ).toEqual(
+      expect.objectContaining({
+        name: 'pnpm',
+        major: 8,
+        minor: 13,
+        patch: 0,
+      }),
+    );
   });
 
   it('Can be stringified', () => {
-    expect(new PackageMangerDetails('npm', '9.2.0').toString()).toEqual(`npm@9.2.0`);
-    expect(new PackageMangerDetails('npm', '').toString()).toEqual(`npm`);
-    expect(new PackageMangerDetails('npm').toString()).toEqual(`npm`);
+    expect(new PackageMangerDetails('pnpm@1.2.3').toString()).toEqual('pnpm@1.2.3');
+    expect(new PackageMangerDetails('pnpm').toString()).toEqual('pnpm');
   });
 
   it('Can be compared', () => {
-    const npm9 = new PackageMangerDetails('npm', '9.0.0');
-    const npm8 = new PackageMangerDetails('npm', '8.0.0');
-    const pnpm7 = new PackageMangerDetails('pnpm', '7.0.0');
+    const npm9 = new PackageMangerDetails('npm@9.0.0');
+    const npm8 = new PackageMangerDetails('npm@8.0.0');
+    const pnpm7 = new PackageMangerDetails('pnpm@7.0.0');
 
     expect(npm8.compare(npm9)).toEqual(ComparisonResults.DifferentVersion);
     expect(npm8.compare(pnpm7)).toEqual(ComparisonResults.DifferentName);
     expect(npm9.compare(npm9)).toEqual(ComparisonResults.Same);
+  });
+
+  describe('PackageMangerDetails.parse()', () => {
+    it('Parsed the packageManager property from package.json', () => {
+      expect(PackageMangerDetails.parse('npm@1.2.3')).toEqual({
+        name: 'npm',
+        version: '1.2.3',
+        major: 1,
+        minor: 2,
+        patch: 3,
+        prerelease: undefined,
+        buildMetaData: undefined,
+      });
+
+      expect(PackageMangerDetails.parse('yarn@1.2.3')).toEqual({
+        name: 'yarn',
+        version: '1.2.3',
+        major: 1,
+        minor: 2,
+        patch: 3,
+        prerelease: undefined,
+        buildMetaData: undefined,
+      });
+
+      expect(PackageMangerDetails.parse('cnpm@1.2.3')).toEqual({
+        name: 'cnpm',
+        version: '1.2.3',
+        major: 1,
+        minor: 2,
+        patch: 3,
+        prerelease: undefined,
+        buildMetaData: undefined,
+      });
+
+      expect(PackageMangerDetails.parse('pnpm@8.13.0')).toEqual({
+        name: 'pnpm',
+        version: '8.13.0',
+        major: 8,
+        minor: 13,
+        patch: 0,
+        prerelease: undefined,
+        buildMetaData: undefined,
+      });
+
+      expect(PackageMangerDetails.parse('pnpm@8.13.0-alpha.1')).toEqual({
+        name: 'pnpm',
+        version: '8.13.0-alpha.1',
+        major: 8,
+        minor: 13,
+        patch: 0,
+        prerelease: 'alpha.1',
+        buildMetaData: undefined,
+      });
+
+      expect(
+        PackageMangerDetails.parse(
+          'pnpm@8.13.0+sha256.fbcf256db6d06bc189e31df34b3ed61220f3ba9f78a2ca8fe7be0fce4670dbd3',
+        ),
+      ).toEqual({
+        name: 'pnpm',
+        version: '8.13.0',
+        major: 8,
+        minor: 13,
+        patch: 0,
+        prerelease: undefined,
+        buildMetaData: 'sha256.fbcf256db6d06bc189e31df34b3ed61220f3ba9f78a2ca8fe7be0fce4670dbd3',
+      });
+    });
+
+    it('Bad data gets undefined values', () => {
+      expect(PackageMangerDetails.parse('bad data')).toEqual({
+        name: undefined,
+        version: undefined,
+        major: undefined,
+        minor: undefined,
+        patch: undefined,
+        prerelease: undefined,
+        buildMetaData: undefined,
+      });
+    });
   });
 });
--- src/PackageMangerDetails.ts
+++ src/PackageMangerDetails.ts
@@ -1,40 +1,72 @@
-import assert from 'node:assert';
+import { ComparisonResults } from './types.js';
 
-import { isVersionString } from './type-predicate.js';
-import { type VersionString, type PackageManagerString, ComparisonResults } from './types.js';
+export interface ParsedPackageManager {
+  name?: string | undefined;
+  version?: string | undefined;
+  major?: number | undefined;
+  minor?: number | undefined;
+  patch?: number | undefined;
+  prerelease?: string | undefined;
+  buildMetaData?: string | undefined;
+}
 
-export class PackageMangerDetails {
-  public readonly name: string;
+export class PackageMangerDetails implements ParsedPackageManager {
+  public readonly name: ParsedPackageManager['name'];
 
-  public readonly version: VersionString | undefined;
+  public readonly version: ParsedPackageManager['version'];
 
-  constructor(name: string, version?: string | undefined) {
-    this.name = name;
+  public readonly major: ParsedPackageManager['major'];
 
-    if (isVersionString(version)) {
-      this.version = version;
-    }
+  public readonly minor: ParsedPackageManager['minor'];
+
+  public readonly patch: ParsedPackageManager['patch'];
+
+  public readonly prerelease: ParsedPackageManager['prerelease'];
+
+  public readonly buildMetaData: ParsedPackageManager['buildMetaData'];
+
+  constructor(input: string | ParsedPackageManager) {
+    const details = typeof input === 'string' ? PackageMangerDetails.parse(input) : input;
+
+    this.name = details.name;
+    this.version = details.version;
+    this.major = details.major;
+    this.minor = details.minor;
+    this.patch = details.patch;
+    this.prerelease = details.prerelease;
+    this.buildMetaData = details.buildMetaData;
   }
 
-  static create(input: PackageManagerString): PackageMangerDetails {
-    const [name, version] = input.split('@');
+  static parse(input: string): ParsedPackageManager {
+    // RegExp modified from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+    const match = input.match(
+      /^(?<name>[pc]?npm|yarn)(?:@(?<version>(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?)(?:\+(?<buildMetaData>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)?$/,
+    );
 
-    assert(name);
+    const { name, version, major, minor, patch, prerelease, buildMetaData } = match?.groups ?? {};
 
-    return new PackageMangerDetails(name, version);
+    return {
+      name,
+      version,
+      major: major ? Number.parseInt(major) : undefined,
+      minor: minor ? Number.parseInt(minor) : undefined,
+      patch: patch ? Number.parseInt(patch) : undefined,
+      prerelease,
+      buildMetaData,
+    };
   }
 
   toString(): string {
-    return `${this.name}${this.version ? `@${this.version}` : ''}`;
+    return `${this.name}${this.version ? '@' + this.version : ''}`;
   }
 
-  compare(details: PackageMangerDetails): ComparisonResults {
-    if (this !== details) {
-      if (this.name !== details.name) {
+  compare(other: PackageMangerDetails): ComparisonResults {
+    if (this !== other) {
+      if (this.name !== other.name) {
         return ComparisonResults.DifferentName;
       }
 
-      if (typeof this.version === 'string' && this.version !== details.version) {
+      if (this.version && this.version !== other.version) {
         return ComparisonResults.DifferentVersion;
       }
     }
